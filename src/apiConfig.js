53cd1860ed101f55c6f8c81c0390aa01887aef1b
wijdan1995 <wijdan.w.k@outlook.sa>
2019-07-29 15:53:29 +0300

    added production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://murmuring-fortress-48285.herokuapp.com',
   development: `http://localhost:${expressPort}`
 }
 
