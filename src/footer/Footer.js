5407a108a0b9813fcfb0af251d77396e8344e8a5
wijdan1995 <wijdan.w.k@outlook.sa>
2019-07-31 12:18:55 +0300

    added footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 
 const Footer = () => (
-    <footer className="footer"> <p className="copyright">
+    <footer className="footer"> <p>
         &copy; 2019 Created with &hearts; by <a href="https://github.com/wijdan1995">Wijdan</a>
     </p></footer>
 );
